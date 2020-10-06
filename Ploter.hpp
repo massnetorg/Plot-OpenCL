5f986b84197a9f18af1e0bc9d39dc13c0b2834b9
Blink <yu574883289@gmail.com>
2020-10-06 17:15:35 +0800

    reverse B in CPU

--- Ploter.hpp
+++ Ploter.hpp
@@ -11,6 +11,7 @@
 #include <thread>
 #include <vector>
 #include <immintrin.h>
+#include "util.h"
 #define MAX_SOURCE_SIZE (0x100000)
 #define MAX_TABLE_A_INPUT_SIZE 536870912 // 2GB of 4 Bytes block
 class Ploter {
@@ -28,7 +29,7 @@ public:
         cl_cmd_q = clCreateCommandQueue(cl_ctx, device_id, 0, &cl_ret);
         cl_input_obj = clCreateBuffer(cl_ctx, CL_MEM_READ_ONLY, sizeof(unsigned int), NULL, &cl_ret);
         cl_pubkey_hash_obj = clCreateBuffer(cl_ctx, CL_MEM_READ_ONLY, 8 * sizeof(unsigned int), NULL, &cl_ret);
-        cl_output_obj = clCreateBuffer(cl_ctx, CL_MEM_WRITE_ONLY, MAX_TABLE_A_INPUT_SIZE * sizeof(unsigned int), NULL, &cl_ret);
+        cl_output_obj = clCreateBuffer(cl_ctx, CL_MEM_READ_WRITE, MAX_TABLE_A_INPUT_SIZE * sizeof(unsigned int), NULL, &cl_ret);
         FILE *fp;
         char *source_str;
         size_t source_size;
@@ -43,19 +44,30 @@ public:
         fclose( fp );
         cl_prg = clCreateProgramWithSource(cl_ctx, 1, (const char **)&source_str, (const size_t *)&source_size, &cl_ret);
         cl_ret = clBuildProgram(cl_prg, 1, &device_id, NULL, NULL, NULL);
-        cl_krnl = clCreateKernel(cl_prg, "sha256_mass_table_a", &cl_ret);
-        cl_ret = clSetKernelArg(cl_krnl, 0, sizeof(cl_mem), (void *)&cl_input_obj);
-        cl_ret = clSetKernelArg(cl_krnl, 1, sizeof(cl_mem), (void *)&cl_pubkey_hash_obj);
-        cl_ret = clSetKernelArg(cl_krnl, 2, sizeof(cl_mem), (void *)&cl_output_obj);
-        global_item_size = MAX_TABLE_A_INPUT_SIZE;
+        cl_krnl_calc_table_a = clCreateKernel(cl_prg, "sha256_mass_table_a", &cl_ret);
+        cl_krnl_calc_table_b = clCreateKernel(cl_prg, "sha256_mass_table_b", &cl_ret);
+        cl_krnl_clean_buffer = clCreateKernel(cl_prg, "clean_buffer", &cl_ret);
+        cl_ret = clSetKernelArg(cl_krnl_calc_table_a, 0, sizeof(cl_mem), (void *)&cl_input_obj);
+        cl_ret = clSetKernelArg(cl_krnl_calc_table_a, 1, sizeof(cl_mem), (void *)&cl_pubkey_hash_obj);
+        cl_ret = clSetKernelArg(cl_krnl_calc_table_a, 2, sizeof(cl_mem), (void *)&cl_output_obj);
+        cl_ret = clSetKernelArg(cl_krnl_calc_table_b, 0, sizeof(cl_mem), (void *)&cl_pubkey_hash_obj);
+        cl_ret = clSetKernelArg(cl_krnl_calc_table_b, 1, sizeof(cl_mem), (void *)&cl_output_obj);
+        cl_ret = clSetKernelArg(cl_krnl_clean_buffer, 0, sizeof(cl_mem), (void *)&cl_output_obj);
+        global_item_size = 4294967232;
+        global_item_size_clean_buf = 536870912;
+        global_item_size_calc_b = 536870912 >> 1;
+        //global_item_size = 536870912;
         group_item_size = 64;
-        A = new unsigned int[1LL << 32];
+        //A = new unsigned int[1LL << 32];
+        B = new uint64_t[1LL << 32];
+        //B_rev = new unsigned int[1LL << 32];
     }
 
     ~Ploter() {
         cl_ret = clFlush(cl_cmd_q);
         cl_ret = clFinish(cl_cmd_q);
-        cl_ret = clReleaseKernel(cl_krnl);
+        cl_ret = clReleaseKernel(cl_krnl_calc_table_a);
+        cl_ret = clReleaseKernel(cl_krnl_clean_buffer);
         cl_ret = clReleaseProgram(cl_prg);
         cl_ret = clReleaseMemObject(cl_input_obj);
         cl_ret = clReleaseMemObject(cl_output_obj);
@@ -70,19 +82,30 @@ public:
         int block_nums = (1LL << bl) / (uint64_t)max_block_size;
         // Use OpenMP to accel
         std::cout << "block_nums: " << block_nums << std::endl;
-        unsigned int *output = new unsigned int[max_block_size];
         auto start = std::chrono::system_clock::now();
         auto end = std::chrono::system_clock::now();
         auto copy_buffer_duration = duration_cast<std::chrono::microseconds>(start - start);
         auto compute_duration = duration_cast<std::chrono::microseconds>(start - start);
         auto store_table_a_duration = duration_cast<std::chrono::microseconds>(start - start);
+        auto compute_b_duration = duration_cast<std::chrono::microseconds>(start - start);
+        //unsigned int *zeros = new unsigned int[MAX_TABLE_A_INPUT_SIZE];
+        //memset (zeros, 0, sizeof(unsigned int) * MAX_TABLE_A_INPUT_SIZE);
         for (int j = 0; j < block_nums; j++) {
+            A = new unsigned int[1 << 29];
+            B_rev = new unsigned int[1 << 29];
             unsigned int offset = MAX_TABLE_A_INPUT_SIZE * j;
             // copy input buffer to device
-            cl_ret = clEnqueueWriteBuffer(cl_cmd_q, cl_input_obj, CL_TRUE, 0, sizeof(unsigned int), &offset, 0, NULL, NULL);
-            // calc
+            //unsigned int zero = 0;
+            //cl_ret = clEnqueueWriteBuffer(cl_cmd_q, cl_output_obj, CL_TRUE, 0, sizeof(unsigned int) * MAX_TABLE_A_INPUT_SIZE, zeros, 0, NULL, NULL);
+            //cl_ret = clEnqueueFillBuffer(cl_cmd_q, cl_output_obj, &zero, sizeof(unsigned int), 0, sizeof(unsigned int) * (size_t)MAX_TABLE_A_INPUT_SIZE, 0, NULL, NULL);
             start = std::chrono::system_clock::now();
-            cl_ret = clEnqueueNDRangeKernel(cl_cmd_q, cl_krnl, 1, NULL,
+            cl_ret = clEnqueueWriteBuffer(cl_cmd_q, cl_input_obj, CL_TRUE, 0, sizeof(unsigned int), &offset, 0, NULL, NULL);
+
+            cl_ret = clEnqueueNDRangeKernel(cl_cmd_q, cl_krnl_clean_buffer, 1, NULL,
+                                            &global_item_size_clean_buf, &group_item_size, 0, NULL, NULL);
+            clFinish(cl_cmd_q);
+            // calc
+            cl_ret = clEnqueueNDRangeKernel(cl_cmd_q, cl_krnl_calc_table_a, 1, NULL,
                                             &global_item_size, &group_item_size, 0, NULL, NULL);
             clFinish(cl_cmd_q);
             end = std::chrono::system_clock::now();
@@ -90,10 +113,28 @@ public:
             // copy output buffer to host
             start = std::chrono::system_clock::now();
             cl_ret = clEnqueueReadBuffer(cl_cmd_q, cl_output_obj, CL_TRUE, 0,
-                                         MAX_TABLE_A_INPUT_SIZE * sizeof(unsigned int), output, 0, NULL, NULL);
+                                         MAX_TABLE_A_INPUT_SIZE * sizeof(unsigned int), A, 0, NULL, NULL);
             end = std::chrono::system_clock::now();
             copy_buffer_duration += duration_cast<std::chrono::microseconds>(end - start);
             start = std::chrono::system_clock::now();
+            cl_ret = clEnqueueNDRangeKernel(cl_cmd_q, cl_krnl_calc_table_b, 1, NULL,
+                                            &global_item_size_calc_b, &group_item_size, 0, NULL, NULL);
+            clFinish(cl_cmd_q);
+            end = std::chrono::system_clock::now();
+            compute_b_duration += duration_cast<std::chrono::microseconds>(end - start);
+            start = std::chrono::system_clock::now();
+            cl_ret = clEnqueueReadBuffer(cl_cmd_q, cl_output_obj, CL_TRUE, 0,
+                                         MAX_TABLE_A_INPUT_SIZE * sizeof(unsigned  int), B_rev, 0, NULL, NULL);
+            clFinish(cl_cmd_q);
+            end = std::chrono::system_clock::now();
+            copy_buffer_duration += duration_cast<std::chrono::microseconds>(end - start);
+
+            start = std::chrono::system_clock::now();
+            make_table_B(A, B_rev);
+            end = std::chrono::system_clock::now();
+            auto make_table_b_duration = duration_cast<std::chrono::microseconds>(end - start);
+            std::cout << "make_table_b: " << double(make_table_b_duration.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den << "s" << std::endl;
+            /*
             for (unsigned int i = 0; i < MAX_TABLE_A_INPUT_SIZE - 4; i++) {
                 //_mm_prefetch(&A[output[i + 1]], _MM_HINT_NTA);
                 _mm_prefetch(&A[output[i + 2]], _MM_HINT_NTA);
@@ -103,19 +144,33 @@ public:
             A[output[MAX_TABLE_A_INPUT_SIZE - 3]] = offset + MAX_TABLE_A_INPUT_SIZE - 3;
             A[output[MAX_TABLE_A_INPUT_SIZE - 2]] = offset + MAX_TABLE_A_INPUT_SIZE - 2;
             A[output[MAX_TABLE_A_INPUT_SIZE - 1]] = offset + MAX_TABLE_A_INPUT_SIZE - 1;
-
+             */
             //write_table_A(output);
-            end = std::chrono::system_clock::now();
-            store_table_a_duration += duration_cast<std::chrono::microseconds>(end - start);
-            for(int i = 0; i < 16; i++) {
-                printf("sha256(%u) = %u\n", i, output[i]);
+
+            printf("%d\n", j);
+            /*
+            int wrong = 0;
+            for(unsigned int i = 0; i < 100000; i++) {
+                if (A[offset + i] != 0) {
+                    if ((offset + i) % 2 == 0) {
+                        if (!verify(A[offset + i], pubkeyHash, (offset + i) >> 1)) {
+                            wrong ++;
+                        }
+                    } else {
+                        if (!verify(A[offset + i], pubkeyHash, ~((offset + i - 1) >> 1))) {
+                            wrong ++;
+                        }
+                    }
+                }
+                //printf("sha256(%u) = %u\n", i, A[offset + i]);
             }
+            std::cout << "wrong: " << wrong << std::endl;
+             */
         }
         std::cout << "compute: " << double(compute_duration.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den << "s" << std::endl;
-        std::cout << "copy_buffser: " << double(copy_buffer_duration.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den << "s" << std::endl;
-        std::cout << "store_A: " << double(store_table_a_duration.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den << "s" << std::endl;
+        std::cout << "copy_buffer: " << double(copy_buffer_duration.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den << "s" << std::endl;
+        std::cout << "compute_b: " << double(compute_b_duration.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den << "s" << std::endl;
 
-        delete [] output;
     }
 
 private:
@@ -157,6 +212,20 @@ private:
         return most_powerful_device;
     }
 
+    void make_table_B(unsigned int *A, unsigned int *B_rev) {
+        unsigned int end = 1 << 29;
+        uint64_t x = 0, xp = 0;
+        uint64_t pos = 0;
+        for (unsigned int i = 0; i < end; i += 2) {
+            x = A[i];
+            xp = A[i + 1];
+            B[B_rev[i]] = x << 32 | xp;
+            B[B_rev[i + 1]] = xp << 32 | x;
+        }
+        delete[] A;
+        delete[] B_rev;
+    }
+
     void write_table_A(unsigned int *output) {
         std::atomic_flag lock[256];
         for (int i = 0; i < 256; i++) {
@@ -190,10 +259,13 @@ private:
     cl_command_queue cl_cmd_q;
     cl_mem cl_input_obj, cl_output_obj, cl_pubkey_hash_obj;
     cl_program cl_prg;
-    cl_kernel cl_krnl;
+    cl_kernel cl_krnl_calc_table_a, cl_krnl_clean_buffer, cl_krnl_calc_table_b;
     size_t global_item_size;
     size_t group_item_size;
+    size_t global_item_size_clean_buf, global_item_size_calc_b;
     unsigned int *A;
+    uint64_t *B;
+    unsigned int *B_rev;
 };
 
 
---
+++ util.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Blink on 2020/10/5.
+//
+
+#include "util.h"
